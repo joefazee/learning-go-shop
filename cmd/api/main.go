d68edd1fee9ce3856a7dea320d04a1a17c87f504
Abah Joseph <joefazee@gmail.com>
2025-08-10 01:36:46 +0200

    add configs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joefazee/learning-go-shop/internal/config"
+	"github.com/joefazee/learning-go-shop/internal/database"
+	"github.com/joefazee/learning-go-shop/internal/logger"
+)
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load config")
+	}
+
+	db, err := database.New(cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to database")
+	}
+
+	mainDB, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get database connection")
+	}
+
+	defer mainDB.Close()
+	gin.SetMode(cfg.Server.GinMode)
+
+	log.Info().Msg("starting server")
 
 }
